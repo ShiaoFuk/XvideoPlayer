f8b2cbf798014e39516af422d7f372a33fa191a1
Rabit <msf1090480147@outlook.com>
2023-11-24 23:19:01 +0800

    new change

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include <iostream>
 
 //#include <opencv2/core/utils/logger.hpp>
@@ -10,32 +11,60 @@
 
 using namespace cv;
 using namespace std;
-void printMs(const char* text = "") {
-	static long long last = 0;
-	long long cur = getTickCount();  // int 64
-	if (last == 0) {
-		last = cur;
-
-		return;
-	}
-	long long ms = 0;
-	ms = (double(cur - last) / getTickFrequency()) * 1000;
-	if (*text != 0) {
-		printf("%s = %dms\n", text, ms);
-	}
-}
+//void printMs(const char* text = "") {
+//	static long long last = 0;
+//	long long cur = getTickCount();  // int 64
+//	if (last == 0) {
+//		last = cur;
+//
+//		return;
+//	}
+//	long long ms = 0;
+//	ms = (double(cur - last) / getTickFrequency()) * 1000;
+//	if (*text != 0) {
+//		printf("%s = %dms\n", text, ms);
+//	}
+//}
 
 
 int main() {
-	VideoCapture video;
-	video.open("X:\\opencv\\build\\x64\\vc16\\bin\\video.mp4");
-	if (video.isOpened() == false) {
-		perror("open video failed!");
+	cv::utils::logging::setLogLevel(utils::logging::LOG_LEVEL_SILENT);  // 通过设置日志打印级别减少不必要的日志输出
+	VideoCapture cam(0);
+	namedWindow("cam");
+	if (!cam.isOpened()) {
+		cout << "cam open failed" << endl;
 		getchar();
 		return -1;
 	}
-	cout << "open video success!" << endl;
+	Mat img;
+	int fps = cam.get(CAP_PROP_FPS);
+	if (fps <= 0) fps = 25;
+	VideoWriter vw;
+	vw.open("out.avi",
+		VideoWriter::fourcc('X', '2', '6', '4'),
+		fps,
+		Size(cam.get(CAP_PROP_FRAME_WIDTH),
+			cam.get(CAP_PROP_FRAME_HEIGHT))
+		);
+	if (!vw.isOpened()) {
+		perror("video open failed");
+		return -1;
+	}
+	for (;;) {
+		cam.read(img);
+		if (img.empty()) {
+			break;
+		}
+		imshow("cam", img);
+		vw.write(img);
+		if (waitKey(5) == 'q') break;  // 按下q退出
+	}
+	vw.release();
+	//imshow()
+	waitKey(0);
+	return 0;
 }
+
 
 //#include <iostream>
 //
